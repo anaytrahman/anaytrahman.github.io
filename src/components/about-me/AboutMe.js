435153354495ae725006c82a213eb46761bbcf52
Anaytullah Rahmani <anaytrahman786@gmail.com>
2021-08-30 12:27:11 +0530

    updated profile

---
+++ src/components/about-me/AboutMe.js
@@ -0,0 +1,24 @@
+import mypic from './../../assets/images/ar-bikee.jpg';
+const AboutMe=()=>{
+    return(
+        <>
+                <div className="aboutMe-container">
+                     <div className="slider-wrapper">
+                          
+                     </div>
+                     {/* <div className="about-wrapper">
+                                    <div className="profile-pic-wrapper">
+                                            <img src={mypic} alt="img"/>
+                                    </div>
+                                    <div className="details-wrapper">
+                                            <h2>
+                                                I am Anayt Rahman
+                                            </h2>
+                                    </div>
+                     </div> */}
+                </div>
+        </>
+    )
+}
+
+export default AboutMe;
---
+++ src/components/mySkils/MySkils.js
@@ -0,0 +1,33 @@
+import './skils.scss';
+const MySkils = () => {
+    return (
+        <div className="skils-container">
+            <h2 className="center">My Skills</h2>
+
+            <p>ReactJs</p>
+            <div class="skils-wrapper">
+                <div class="skills html">90%</div>
+            </div>
+
+
+            <p>JavaScript</p>
+            <div class="skils-wrapper">
+                <div class="skills css">80%</div>
+            </div>
+
+
+            <p>HTML5</p>
+            <div class="skils-wrapper">
+                <div class="skills html">90%</div>
+            </div>
+
+            <p>CSS3</p>
+            <div class="skils-wrapper">
+                <div class="skills html">90%</div>
+            </div>
+
+        </div>
+    )
+}
+
+export default MySkils;
